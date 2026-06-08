61f95518efc67f4132ea1dc34bb9c8cb2bbb8056
Samuel Molano <samuel.molano7@gmail.com>
2026-06-08 15:31:52 +0200

    Text change

--- script/ACTG320_hyperopt_aug_traincontrol_parallel.py
+++ script/ACTG320_hyperopt_aug_traincontrol_parallel.py
@@ -80,7 +80,7 @@ def run(generator_name):
         df_init_control["treatment"] = 0
        
         # Parameters of the optuna study
-        metric_optuna = "survival_km_distance" # metric to{} optimize in optuna
+        metric_optuna = "survival_km_distance" # metric to optimize in optuna
         method_hyperopt = "train_full_gen_full"
         n_splits = 5 # number of splits for cross-validation
         n_generated_dataset = 200 # number of generated datasets per fold to compute the metric
