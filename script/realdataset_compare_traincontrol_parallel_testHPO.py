2b7f97d6164144b69805ab3e5132c8d4410274d7
Perrine Chassat <perrine.chassat@gmail.com>
2026-06-23 10:12:00 +0200

    fix

--- script/realdataset_compare_traincontrol_parallel_testHPO.py
+++ script/realdataset_compare_traincontrol_parallel_testHPO.py
@@ -186,9 +186,8 @@ if __name__ == "__main__":
     # generators_sel = ["HI-VAE_weibull", "HI-VAE_piecewise", "Surv-GAN", "Surv-VAE", "HI-VAE_weibull_prior", "HI-VAE_piecewise_prior"]
     dataset_sel = ["ACTG320", "NCT00119613", "NCT00113763", "NCT00339183"]
     generators_sel = ["HI-VAE_weibull", "HI-VAE_piecewise"]
-    # dataset_sel = ["NCT00113763"]
     dataset_id = int(sys.argv[1])
     dataset_name = dataset_sel[dataset_id]
-    method_HPO_list = ["DetectXGB", "ValLoss", "SurvDist"] #, "Kmap_SurvDist", "IdfScore_SurvDist"]
-    for method_HPO in method_HPO_list:
-        run(dataset_name , generators_sel, method_HPO)
+    method_HPO = "ValLoss" # "DetectXGB" #, "ValLoss", "SurvDist"] #, "Kmap_SurvDist", "IdfScore_SurvDist"]
+    # for method_HPO in method_HPO_list:
+    run(dataset_name , generators_sel, method_HPO)
